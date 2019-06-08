2e69d3df489fdcb9690290507523db15062e02c5
mohan-dev1248 <mohanakrishna96@gmail.com>
2019-06-08 18:27:08 +0530

    Moved data related code inside GameViewModel from GameFragment

--- GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -9,8 +9,72 @@ class GameViewModel: ViewModel() {
         const val TAG = "GameViewModel"
     }
 
+    private lateinit var wordList: MutableList<String>
+
+    // The current word
+    var word = ""
+
+    // The current score
+    var score = 0
+
     init {
         Log.i(TAG, "GameViewModel Created!")
+        resetList()
+        nextWord()
+    }
+
+    /**
+     * Resets the list of words and randomizes the order
+     */
+    private fun resetList() {
+        wordList = mutableListOf(
+                "queen",
+                "hospital",
+                "basketball",
+                "cat",
+                "change",
+                "snail",
+                "soup",
+                "calendar",
+                "sad",
+                "desk",
+                "guitar",
+                "home",
+                "railway",
+                "zebra",
+                "jelly",
+                "car",
+                "crow",
+                "trade",
+                "bag",
+                "roll",
+                "bubble"
+        )
+        wordList.shuffle()
+    }
+
+    fun onSkip() {
+        if (!wordList.isEmpty()) {
+            score--
+        }
+        nextWord()
+    }
+
+    fun onCorrect() {
+        if (!wordList.isEmpty()) {
+            score++
+        }
+        nextWord()
+    }
+
+    /**
+     * Moves to the next word in the list
+     */
+    private fun nextWord() {
+        if (!wordList.isEmpty()) {
+            //Select and remove a word from the list
+            word = wordList.removeAt(0)
+        }
     }
 
     override fun onCleared() {
